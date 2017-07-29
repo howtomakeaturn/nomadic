b4ccc54934adcf78593de329a619fa0a89a7aaa4
chuan-hao, you <howtomakeaturn@hotmail.com>
2017-07-29 14:41:19 +0800

    remove the wrong GA code

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -361,17 +361,6 @@
         @yield('content')
 
         <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'UA-85456048-2', 'auto');
-          ga('send', 'pageview');
-
-        </script>
-
-        <script>
             $(document).ready(function(){
                 var cafeId = window.location.hash.substr(1);
 
