f23146e2420bc87dd7292abbb173bf3acc944d25
howtomakeaturn <howtomakeaturn@hotmail.com>
2018-04-19 15:22:37 +0800

    enable the original routes!

--- Http/routes.php
+++ Http/routes.php
@@ -2,7 +2,7 @@
 
 Route::group(['middleware' => 'web', /*'prefix' => 'nomadicore', */'namespace' => 'Modules\NomadiCore\Http\Controllers'], function()
 {
-    Route::get('/', 'NomadiCoreController@index');
+    // Route::get('/', 'NomadiCoreController@index');
 
     Route::get('/', 'HomepageController@index');
 
