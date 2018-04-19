b146d550f814cdc80156f407dedcab62333989ab
howtomakeaturn <howtomakeaturn@hotmail.com>
2018-04-19 15:15:14 +0800

    move city home, list, and map pages into modules

--- Http/routes.php
+++ Http/routes.php
@@ -7,4 +7,12 @@ Route::group(['middleware' => 'web', /*'prefix' => 'nomadicore', */'namespace' =
     Route::get('/', 'HomepageController@index');
 
     Route::get('/home', 'HomepageController@home');
+
+    foreach (Config::get('city') as $key => $value) {
+        Route::get('/{' . $key . '}', 'CityController@getHomepage');
+
+        Route::get('/{' . $key . '}/list', 'CityController@getList');
+
+        Route::get('/{' . $key . '}/map', 'CityController@getMap');
+    }
 });
