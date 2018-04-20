c294c83be6ce4bc3d67eac69dc5f23010c8e9a61
howtomakeaturn <howtomakeaturn@hotmail.com>
2018-04-20 11:09:31 +0800

    move

--- Http/routes.php
+++ Http/routes.php
@@ -4,6 +4,85 @@ Route::group(['middleware' => 'web', /*'prefix' => 'nomadicore', */'namespace' =
 {
     // Route::get('/', 'NomadiCoreController@index');
 
+    Route::get('/login', function(){
+        Session::put('cafe_id', Request::get('cafe_id'));
+
+        Session::put('path', Request::get('path'));
+
+        Session::put('action', Request::get('action'));
+
+        return Socialite::driver('facebook')->redirect();
+    });
+
+    Route::get('/callback', function(){
+
+        if (Request::get('error_reason') == 'user_denied') {
+            return redirect(Session::get('path'));
+        }
+
+        $facebook = Socialite::driver('facebook')->user();
+
+        if ($credential = App\SocialCredential::where('social_id', $facebook->id)->first()) {
+
+            Auth::loginUsingId($credential->user_id);
+
+        } else {
+
+            $user = new App\User();
+
+            $email = $facebook->getEmail() ? $facebook->getEmail() : $facebook->getId() . '@facebook.com';
+
+            $user->email = $email;
+
+            $user->name = $facebook->getName();
+
+            $user->password = '';
+
+            $user->save();
+
+            $profile = new App\Profile();
+
+            $profile->user_id = $user->id;
+
+            $profile->avatar = str_replace('type=normal', 'type=square', $facebook->getAvatar());
+
+            $profile->save();
+
+            $credential = new App\SocialCredential();
+
+            $credential->user_id = $user->id;
+
+            $credential->social_id = $facebook->id;
+
+            $credential->save();
+
+            Auth::login($user);
+
+        }
+
+        if (Session::get('action')=='recommend') {
+            $rec = App\Recommendation::where('cafe_id', Session::get('cafe_id'))
+                ->where('user_id', Auth::user()->id)
+                ->first();
+
+            if (!$rec) {
+                $rec = new App\Recommendation();
+                $rec->cafe_id = Session::get('cafe_id');
+                $rec->user_id = Auth::user()->id;
+                $rec->save();
+            }
+        }
+
+        return redirect(Session::get('path'). '#' . Session::get('cafe_id'));
+    });
+
+    Route::get('/logout', function(){
+
+        Auth::logout();
+
+        return redirect('/');
+    });
+
     Route::get('/', 'HomepageController@index');
 
     Route::get('/home', 'HomepageController@home');
