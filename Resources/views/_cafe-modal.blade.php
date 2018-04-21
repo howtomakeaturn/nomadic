4d2910e1d43529948d59eb3fd378d616eb51a02c
howtomakeaturn <howtomakeaturn@hotmail.com>
2018-04-21 14:07:06 +0800

    update entity modal style

--- Resources/views/_cafe-modal.blade.php
+++ Resources/views/_cafe-modal.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="modal-{{$entity->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" data-uuid="{{ $entity->id }}" data-keyboard="false">
+<div class="modal cafe-modal" id="modal-{{$entity->id}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" data-uuid="{{ $entity->id }}" data-keyboard="false">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
